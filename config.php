3377c023cc8e71d460a41921999c5592e9b5062d
Daytime <416697768@qq.com>
2016-11-29 16:52:03 +0800

    web code

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+    define("WebRoot", dirname(__FILE__),false);
+    define("WebUrl", "http://localhost/Grade");
+    define("WebDomain", "localhost");
+    error_reporting(E_ALL^E_NOTICE);
+?>
